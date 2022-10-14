48a65fafcaf125b9b76be6977a58d1a6c6c87e67
Benja <bermanschultz29@gmail.com>
2022-10-13 21:42:35 -0300

    init project

---
+++ src/main/java/com/tccspringboot/tccspringboot/TccspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tccspringboot.tccspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TccspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TccspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tccspringboot/tccspringboot/TccspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tccspringboot.tccspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
